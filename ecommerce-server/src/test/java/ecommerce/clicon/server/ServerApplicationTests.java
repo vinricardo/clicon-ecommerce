7087abcb1e5ae1f58ee4c0503c8c4d0ca78fa0a5
Vinicius <vinicius.01deagosto@gmail.com>
2024-10-04 10:48:42 -0300

    support: iniciando back

---
+++ ecommerce-server/src/test/java/ecommerce/clicon/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.clicon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
